15e73c3a173ebd6af356402f9a6f2ac960c6f7f8
bluedragon1988 <408055748@qq.com>
2023-09-17 10:46:34 +0800

    上传项目目录

---
+++ MainProject/goutou2/Assets/Scripts/UI/Test.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public void OnTestClick()
+    {
+        Debug.Log("点了一下你爹");
+    }
+}
